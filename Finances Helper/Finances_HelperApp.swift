cfa7427b67538bfd617ea6729cd67f023811e98f
BogdanZyk <zykovbogdan07@gmail.com>
2023-03-31 13:09:57 +0300

    Initial Commit

---
+++ Finances Helper/Finances_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finances_HelperApp.swift
+//  Finances Helper
+//
+//  Created by Bogdan Zykov on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Finances_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
